f284c930dfc403ac4bfb4a5c6fe29fd8fb23cb52
Alex <alexs237044@gmail.com>
2026-08-10 00:50:14 -0400

    Feat: Find solution, but no optimal guarantee

--- main.cpp
+++ main.cpp
@@ -1,25 +1,37 @@
-#include <algorithm>
+#include <cctype>
+#include <cstddef>
 #include <fstream>
-#include <functional>
 #include <iostream>
-#include <iterator>
-#include <map>
 #include <string>
 #include <vector>
 
 using std::vector;
 
-void get_poomap( std::string filename, vector<std::string> wordlist ) {
+struct word {
+	std::string			   word;
+	int					   dist;
+	vector<struct word *> *upstream;
+} typedef word;
+
+void get_poomap( std::string filename, vector<word *> *wl ) {
 	std::ifstream words( filename );
-	wordlist.resize( 1 );
-	for ( int i = 0; std::getline( words, wordlist[i] ); ++i )
-		wordlist.resize( i + 2 );
+	std::string	  buff;
+	for ( int i = 0; std::getline( words, buff ); ++i ) {
+		word *w = new word;
+		w->word += buff;
+		w->dist = -1;
+		if ( buff.compare( "poop" ) == 0 ) w->dist = 0;
+		w->upstream = new vector<word *>;
+		wl->push_back( w );
+	}
 	words.close();
 }
 
-template <typename T> inline bool in_vec( vector<T> vec, T item ) {
-	return std::find( std::begin( vec ), std::end( vec ), item ) !=
-		   std::end( vec );
+int in_vec( word *w, vector<word *> *wl ) {
+	for ( int i = 0; i < wl->size(); ++i ) {
+		if ( !w->word.compare( ( *wl )[i]->word ) ) return i;
+	}
+	return -1;
 }
 
 bool one_diff( std::string w1, std::string w2 ) {
@@ -29,28 +41,42 @@ bool one_diff( std::string w1, std::string w2 ) {
 	return diff == 1;
 }
 
-void poosolve( std::string word, const vector<std::string> wordlist ) {
-	if ( in_vec( wordlist, word ) ) {
+void poosolve( word *word, vector<struct word *> *wl ) {
+	int pos = in_vec( word, wl );
+	if ( pos == -1 ) {
 		std::cout << "Word not in list!\n";
 		return;
 	}
-	vector<int> dist;
-	int			nz = 0;
-	for ( auto i = 0; i < wordlist.size(); ++i ) dist.push_back( 0 );
-	for ( int i = 0; i < sizeof( dist ); ++i )
-		if ( one_diff( wordlist[i], "poop" ) ) {
-			nz++;
-			dist[i] = 1;
+	std::cout << "Word in list, finding solution...\n";
+
+	int done = 0;
+	for ( int i = 0; i < wl->size(); ++i ) {
+		if ( one_diff( "POOP", ( *wl )[i]->word ) ) {
+			( *wl )[i]->dist = 1;
+			done++;
 		}
-	while ( nz < sizeof( dist ) )
-		for ( int i = 1; i < sizeof( dist ); ++i )
-			if ( dist[i] != 0 )
-				for ( int j = 0; j < sizeof( dist ); ++j )
-					if ( one_diff( wordlist[i], wordlist[j] ) &&
-						 dist[j] == 0 ) {
-						dist[j] = dist[i] + 1;
-						nz ++;
+	}
+	while ( done < wl->size() )
+		for ( int i = 0; i < wl->size(); ++i )
+			if ( ( *wl )[i]->dist >= 0 )
+				for ( int k = 0; k < wl->size(); ++k )
+					if ( one_diff( ( *wl )[i]->word, ( *wl )[k]->word ) &&
+						 ( *wl )[k]->dist == -1 ) {
+						( *wl )[k]->upstream->push_back( ( *wl )[i] );
+						( *wl )[k]->dist = ( *wl )[i]->dist + 1;
+						done++;
 					}
+	std::cout << "Traced Words\n";
+	std::cout << "Word " + word->word + " has an optimal solution of "
+			  << ( *wl )[pos]->dist << "\n";
+
+	struct word *s = ( *wl )[pos];
+	while ( true ){
+		std::cout << s->word + " -> ";
+		if(s->upstream->size() == 0 ) break;
+		s = ( *s->upstream )[0];
+	}
+	std::cout << "POOP\n";
 }
 
 int main( int argc, char **argv ) {
@@ -58,9 +84,13 @@ int main( int argc, char **argv ) {
 		std::cout << "Not enough arguments!\n";
 		return 0;
 	}
-	vector<std::string> wordlist;
+	vector<word *> *wordlist = new vector<word *>;
 	get_poomap( "poople_words.txt", wordlist );
-	poosolve( argv[1], wordlist );
+	word *arg = new word{ .word = argv[1] };
+	for ( auto &c : arg->word ) c = toupper( c );
+	std::cout << "word: " << arg->word << std::endl;
+
+	poosolve( arg, wordlist );
 
 	return 0;
 }
